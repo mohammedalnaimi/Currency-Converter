7b03e33b05e70d696221659a9652919223c71777
mohammadalnaimi <80594660+mohammadalnaimi@users.noreply.github.com>
2022-11-03 16:14:56 +0300

    Creat Currency Converter

---
+++ main.js
@@ -0,0 +1,19 @@
+fetch(
+  "https://api.currencyfreaks.com/latest?apikey=8392844157b5440e9cdd05cafc5a06d0"
+).then((result) => {
+  let data = result.json()
+  console.log(data)
+  return data
+}).then ((currency) => {
+  // console.log(currency.rates)
+
+  let button = document.getElementById("button")
+  let priceOne = document.querySelector(".tl span")
+  let priceTwo = document.querySelector(".iq span")
+  // console.log(Math.min(currency.rates["IQD"]))
+
+  button.onclick = function () {
+  priceOne.innerHTML = parseFloat(document.getElementById("amount").value * currency.rates["TRY"]).toFixed(2)
+  priceTwo.innerHTML = Math.floor(document.getElementById("amount").value * currency.rates["IQD"])
+  }
+});
