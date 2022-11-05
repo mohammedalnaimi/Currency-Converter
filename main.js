00c89dc0774b8e961d4691a8c9996939384f8e20
mohammadalnaimi <80594660+mohammedalnaimi@users.noreply.github.com>
2022-11-05 15:28:40 +0300

    added euro

--- main.js
+++ main.js
@@ -10,10 +10,12 @@ fetch(
   let button = document.getElementById("button")
   let priceOne = document.querySelector(".tl span")
   let priceTwo = document.querySelector(".iq span")
+  let priceThree = document.querySelector(".euro span")
   // console.log(Math.min(currency.rates["IQD"]))
 
   button.onclick = function () {
-  priceOne.innerHTML = parseFloat(document.getElementById("amount").value * currency.rates["TRY"]).toFixed(2)
-  priceTwo.innerHTML = Math.floor(document.getElementById("amount").value * currency.rates["IQD"]).toFixed(2)
+  priceOne.innerHTML = (document.getElementById("amount").value * currency.rates["TRY"]).toFixed(2)
+  priceTwo.innerHTML = (document.getElementById("amount").value * currency.rates["IQD"]).toFixed(2)
+  priceThree.innerHTML = (document.getElementById("amount").value * currency.rates["EUR"]).toFixed(2)
   }
 });
